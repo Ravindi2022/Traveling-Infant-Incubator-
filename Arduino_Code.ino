99a88b105aae12f9b84bdf7be8d710a28031ccb8
Nadil Lokuhetty <121077619+Nadil2022@users.noreply.github.com>
2025-02-17 23:13:36 +0530

    Create Arduino_Code.ino

---
+++ Arduino_Code.ino
@@ -0,0 +1,41 @@
+#define BLYNK_PRINT Serial
+
+#define BLYNK_TEMPLATE_ID "TMPL6jX5uYplK"
+#define BLYNK_TEMPLATE_NAME "Dht11"
+#define BLYNK_AUTH_TOKEN "4v1zUzuR5XDsHoR74-LtPFY2xYZ5bZE4"
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp32.h>
+
+#include <dht11.h>
+#include <Blynk.h>
+// #include <WiFi.h>
+#include "MAX30100_PulseOximeter.h"
+#include <Wire.h>
+// #include <WiFiClient.h>
+
+#define REPORTING_PERIOD_MS 1000
+#define REPORTING_PERIOD_MS_2 2000
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char auth[] = "4v1zUzuR5XDsHoR74-LtPFY2xYZ5bZE4";
+char ssid[] = "a12";
+char pass[] = "abcd1234";
+
+PulseOximeter pox;
+
+float BPM, SpO2;
+uint32_t tsLastReport = 0;
+uint32_t tsLastReport2 = 0;
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+dht11 DHT11;
+BlynkTimer timer;
+
+void onBeatDetected() {
+  Serial.println("Beat Detected!");
+}
+
